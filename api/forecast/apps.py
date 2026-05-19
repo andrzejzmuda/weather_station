42e3ff7f4c8fafc43f406cbb4241b2383e699ef5
andrzejzmuda <andrzejzmuda@hotmail.com>
2026-05-19 16:19:21 +0200

    scheduler instances added

--- api/forecast/apps.py
+++ api/forecast/apps.py
@@ -12,9 +12,9 @@ class ForecastConfig(AppConfig):
             from forecast.scheduler.get_forecast import (
                 get_current, get_daily, get_hourly, get_minutely_15)
             scheduler = BackgroundScheduler()
-            scheduler.add_job(get_current, 'interval', minutes=15)
-            scheduler.add_job(get_minutely_15, 'interval', hours=1)
-            scheduler.add_job(get_hourly, 'interval', hours=4)
-            scheduler.add_job(get_daily, 'interval', hours=8)
+            scheduler.add_job(get_current, 'interval', minutes=15, max_instances=1)
+            scheduler.add_job(get_minutely_15, 'interval', hours=1, max_instances=1)
+            scheduler.add_job(get_hourly, 'interval', hours=4, max_instances=1)
+            scheduler.add_job(get_daily, 'interval', hours=8, max_instances=1)
             scheduler.start()
             self.scheduler_started = True
